693c811270a2ca032ddb01866c5e5667a4285664
Raghu Varma R <rrv461@gmail.com>
2016-07-14 02:45:37 +0530

    refactor code in problems_controller

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -11,21 +11,6 @@ class ProblemsController < ApplicationController
 		end
 	end
 
-	# def update
-
-	# 	respond_to do |format|
-	#       if @problem.update(application_params)
-	#         # format.html { redirect_to @application, notice: 'Application was successfully updated.' }
-	#         # format.json { render :show, status: :ok, location: @application }
-	#         redirect_to @application, notice: "Problem created."
-	#       else
-	#         # format.html { render :edit }
-	#         # format.json { render json: @application.errors, status: :unprocessable_entity }
-	#         redirect_to @application, alert: "Problem not created."
-	#       end
-	#     end
-	# end
-
 	private
 	def problem_params
       params.require(:problem).permit(:problem_type, :description)
