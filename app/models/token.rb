31fa0494beea2779c6214f62c7ddcb04e8428f6b
Raghu Varma R <rrv461@gmail.com>
2016-07-15 01:50:04 +0530

    added validation for application id with created applications id

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,7 +1,8 @@
 class Token < ActiveRecord::Base
 	validates_presence_of :name
 	validates_presence_of :relation_name
-	validates_presence_of :application_id, numericality: true, :unless => :is_new_application?
+	validates :application_id, numericality: true, inclusion: { in: Application.all.map{|a| a.id},
+    message: "%{value} is not a valid Application Number" }, :unless => :is_new_application?
   belongs_to :application
   DOCTORS = User.where("role_id IN (?)", [2,5,8]).collect{|d| d.name}  
 end
